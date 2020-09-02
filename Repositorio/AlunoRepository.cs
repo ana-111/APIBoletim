4e6569bab218eb2ffd082f558b8f6bf72f4c8103
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-09-01 21:26:01 -0300

    APIBoletim Iniciado

---
+++ Repositorio/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using APIBoletim.Domains;
+using APIBoletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositorio
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno AlterarAluno(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
